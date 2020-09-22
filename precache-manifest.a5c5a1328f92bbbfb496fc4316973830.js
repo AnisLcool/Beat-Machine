3802a2feb8bc895ca5e21f0815563c6326a45145
AnisLcool <42942454+AnisLcool@users.noreply.github.com>
2020-09-22 17:01:36 +0100

    Updates

---
+++ precache-manifest.a5c5a1328f92bbbfb496fc4316973830.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37c464d8bb159f7085beed6d9e8b1609",
+    "url": "/Beat-Machine/index.html"
+  },
+  {
+    "revision": "7a46d1be788bfd7e9e0d",
+    "url": "/Beat-Machine/static/css/main.a824377c.chunk.css"
+  },
+  {
+    "revision": "5eee9dd13385a6be9370",
+    "url": "/Beat-Machine/static/js/0.847f9bf4.chunk.js"
+  },
+  {
+    "revision": "2c32cf4d159d1905f76a",
+    "url": "/Beat-Machine/static/js/3.cb97df9f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Beat-Machine/static/js/3.cb97df9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a46d1be788bfd7e9e0d",
+    "url": "/Beat-Machine/static/js/main.3c13f0e7.chunk.js"
+  },
+  {
+    "revision": "7a34d5dd292fbc6638f3",
+    "url": "/Beat-Machine/static/js/runtime-main.8dcb2ccd.js"
+  }
+]);
